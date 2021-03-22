9b48fed5eba65d62cd923daf8c152a90b86708a6
campbeo2 <80653101+campbeo2@users.noreply.github.com>
2021-03-22 16:25:07 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,5 +1,7 @@
 package CSE201;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,44 +9,75 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Account {
+import javax.swing.JFrame;
+
+public class Account extends JFrame implements ActionListener {
 	String name, email, dob;
 	private String pswd;
-	
+
+	public static void main(String[] args) {
+		new LoginPage();
+
+	}
+
 	Account(String name, String email, String pswd, String dob) {
 		this.name = name;
 		this.email = email;
 		this.pswd = pswd;
 		this.dob = dob;
 	}
-	
-	
+
 	public String getName() {
 		return this.name;
 	}
+
 	public String getEmail() {
 		return this.email;
 	}
+
 	public String getPswd() {
 		return this.pswd;
 	}
+
 	public String getDob() {
 		return this.dob;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
 
 class Artist {
-	
-	
+
 }
 
 class Listener {
-	
+
 }
 
 class Admin {
-	
+
 	public void editAccount() {
-		
+
+	}
+
+}
+
+class HomePage {
+	
+	HomePage() {
+		JFrame homePage;
+
+		homePage = new JFrame();
+
+		homePage.setTitle("GoodTunez Home");
+		homePage.setSize(650, 450);
+		homePage.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		homePage.setVisible(true);
+
+		homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
