6f0b29113b1ec6b7b03042a1d32b68226f8b251c
Nick-Prokup <64275638+Nick-Prokup@users.noreply.github.com>
2021-03-29 12:55:52 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -42,6 +42,83 @@ public class Account extends JFrame implements ActionListener {
 	public String getDob() {
 		return this.dob;
 	}
+	
+	/**
+	 * Base home page method
+	 */
+	public static void HomePage() {
+		JFrame homePage;
+		JPanel acctPanel = new JPanel(new GridLayout(5,1)), panel = new JPanel(), searchPanel = new JPanel(new GridLayout(3,1)),
+				songsPanel = new JPanel(), mainPanel = new JPanel(new BorderLayout());
+		JLabel genre, era, songs, account, search;
+		
+		homePage = new JFrame();
+		
+		homePage.add(mainPanel);
+
+		// display labels 
+		genre = new JLabel("Search by Genre:");
+		era = new JLabel("Search by Era:");
+		songs = new JLabel("Liked Songs: \t\t\t");
+		account = new JLabel("Your Account: ");
+		search = new JLabel("Search for song: ");
+		
+		// define options
+		String[] genres = { "Alernative", "Country", "Hip Hop / Rap", "Rock" };
+		final JComboBox<String> genreBox = new JComboBox<String>(genres);
+		
+		JButton profile = new JButton("Name");
+		JButton type = new JButton("User Type");
+		JButton playlists = new JButton("Playlists");
+		JButton albums = new JButton("Albums?");
+		JButton displayBox = new JButton("Display Data Base Info Here");
+		
+		// add box
+		genreBox.setVisible(true);
+				
+		JButton searchB = new JButton("Search");
+		
+		
+		String[] eras = { "70s", "80s", "90s", "2000's", "2010s", "Current" };
+		final JComboBox<String> eraBox = new JComboBox<String>(eras);
+		
+		// add boxes and panels
+		eraBox.setVisible(true);
+		
+		// add the variables to the panels
+		panel.add(genre);
+		panel.add(genreBox);
+		panel.add(searchB);
+		panel.add(era);
+		panel.add(eraBox);
+		panel.add(searchB);
+		
+		songsPanel.add(songs);
+		
+		acctPanel.add(account);
+		acctPanel.add(profile);
+		acctPanel.add(type);
+		acctPanel.add(playlists);
+		acctPanel.add(albums);
+		
+		searchPanel.add(search);
+		searchPanel.add(new JTextField());
+		searchPanel.add(displayBox);
+
+		
+		// homePage panel adding
+		mainPanel.add(panel, BorderLayout.NORTH);
+		mainPanel.add(songsPanel, BorderLayout.EAST);
+		mainPanel.add(acctPanel, BorderLayout.WEST);
+		mainPanel.add(searchPanel, BorderLayout.SOUTH);
+		
+		homePage.setTitle("GoodTunez Home");
+		homePage.setSize(650, 450);
+		homePage.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		homePage.setVisible(true);
+
+		homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -66,18 +143,4 @@ class Admin {
 
 }
 
-class HomePage {
-	
-	HomePage() {
-		JFrame homePage;
-
-		homePage = new JFrame();
-
-		homePage.setTitle("GoodTunez Home");
-		homePage.setSize(650, 450);
-		homePage.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		homePage.setVisible(true);
-
-		homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
 }
