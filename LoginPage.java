d10c9cb93eccd2a8bc6bd137c7f2212d5b966ebf
campbeo2 <80653101+campbeo2@users.noreply.github.com>
2021-04-13 14:29:51 -0400

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -10,13 +10,15 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 public class LoginPage extends JFrame implements ActionListener {
-	// declare JFrame component variables 
+	// declare JFrame component variables
 	JFrame logFrame;
 	JPanel panel;
 	JLabel email_label, pswd_label, out, empty;
 	JTextField email_text;
 	JPasswordField pswd_text;
 	JButton submit, newAcc;
+
+	static Account currUser;
 
 	// login page declaration, upon opening the class this frame will be displayed
 	LoginPage() {
@@ -27,9 +29,9 @@ public class LoginPage extends JFrame implements ActionListener {
 		pswd_text = new JPasswordField();
 
 		// ***** not sure what JLabel does *****
-		// 
+		//
 		out = new JLabel();
-		// JButton to attempt a login 
+		// JButton to attempt a login
 		submit = new JButton("Log in");
 		empty = new JLabel("");
 		// JButton to create new account
@@ -37,19 +39,19 @@ public class LoginPage extends JFrame implements ActionListener {
 		// creates the spaces and format for the login page panel layout
 		panel = new JPanel(new GridLayout(4, 2));
 
-		// add the variables to the display panel 
+		// add the variables to the display panel
 		panel.add(email_label);
 		panel.add(email_text);
 		panel.add(pswd_label);
 		panel.add(pswd_text);
 
-		// adds the labels and buttons to panel 
+		// adds the labels and buttons to panel
 		panel.add(out);
 		panel.add(submit);
 		panel.add(empty);
 		panel.add(newAcc);
 
-		// sets border around panel 
+		// sets border around panel
 		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
 		// adds button functionality to the login button
@@ -58,7 +60,7 @@ public class LoginPage extends JFrame implements ActionListener {
 				String email = email_text.getText();
 				String pswd = pswd_text.getText();
 
-				// verification of the login credentials 
+				// verification of the login credentials
 				if (email.equals("admin") && pswd.equals("admin") || loginVerification(email, pswd)) {
 					out.setText("Login confirmed");
 					logFrame.dispose();
@@ -92,27 +94,28 @@ public class LoginPage extends JFrame implements ActionListener {
 	/**
 	 * method to verify login credentials
 	 * 
-	 * @param email, String entered by the user which is the email 
-	 * @param pswd, String entered by the user which is the password
-	 * @return boolean, true if account exists and is correct 
+	 * @param email, String entered by the user which is the email
+	 * @param pswd,  String entered by the user which is the password
+	 * @return boolean, true if account exists and is correct
 	 */
 	public boolean loginVerification(String email, String pswd) {
 		// ***** Not sure about bufferedReader
 		try (BufferedReader in = new BufferedReader(new FileReader("AccountList.txt"))) {
 			String line;
 			String[] accInfo;
-			// while there is still data in the account list 
+			// while there is still data in the account list
 			while ((line = in.readLine()) != null) {
 				// reading each account by commas to find email and password as an array element
 				accInfo = line.split(",");
+
 				if (accInfo[4].equals("Listener")) {
-					Listener currUser = new Listener(accInfo[0], accInfo[1], accInfo[2], accInfo[3]);
+					currUser = new Account(accInfo[0], accInfo[1], accInfo[2], accInfo[3], "Listener");
 				} else if (accInfo[4].equals("Artist")) {
-					Artist currUser = new Artist(accInfo[0], accInfo[1], accInfo[2], accInfo[3]);
+					currUser = new Account(accInfo[0], accInfo[1], accInfo[2], accInfo[3], "Artist");
 				}
-				
+
 				if (accInfo[1].equals(email) && accInfo[2].equals(pswd)) {
-					
+
 					return true;
 				}
 			}
