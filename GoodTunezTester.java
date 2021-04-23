df82ee65c5ba82f00cea12ad8b0ab058208b6e80
Nick-Prokup <64275638+Nick-Prokup@users.noreply.github.com>
2021-04-23 16:58:40 -0400

    Update GoodTunezTester.java

--- GoodTunezTester.java
+++ GoodTunezTester.java
@@ -6,32 +6,53 @@ import org.junit.jupiter.api.Test;
 
 class GoodTunezTester {
 
+	// WHITE BOX TESTS BELOW
+	
+	// Tester to check the functionality
+
+	// GREY BOX TESTS BELOW
+	
+	// Tester method to insure that the song function creates new songs correctly
+	@Test 
+	void testSong() {
+		Song test = new Song("Doomed", "NBA Youngboy", "Until I Return", 2021, "Hip Hop / Rap");
+		
+		assertEquals(test.toString(), "Doomed,NBA Youngboy,Until I Return,2021,Hip Hop / Rap");
+		
+		assertEquals(test.getName(), "Doomed");
+		assertEquals(test.getArtist(), "NBA Youngboy");
+		assertEquals(test.getAlbum(), "Until I Return");
+		assertEquals(test.getYear(), 2021);
+		assertEquals(test.getGenre(), "Hip Hop / Rap");
+		
+	}
+	
 	// Tester method to identify that the attributes of the account (Grey)
-	@Test
-	void testListner() {
-		Account l1 = new Account("Owen Campbell", "yes.com", "pswd", "05/02/2002", "Listener");
+		@Test
+		void testListner() {
+			Account l1 = new Account("Owen Campbell", "yes.com", "pswd", "05/02/2002", "Listener");
+			
+			System.out.println(l1.getClass());
+			
+			assertEquals(l1.getName(), "Owen Campbell");
+			assertEquals(l1.getEmail(), "yes.com");
+			assertEquals(l1.getPswd(), "pswd");
+			assertEquals(l1.getDob(), "05/02/2002");
+			
+		}
 		
-		System.out.println(l1.getClass());
+		// Tester method to identify that the attributes of the artists (Grey) 
+		@Test
+		void testArtist() {
+			Account a1 = new Account("John Mayer", "jMayer.com", "pswd", "12/01/1994", "Artist");
+			System.out.println(a1.getClass());
+			assertEquals(a1.getName(), "John Mayer");
+			assertEquals(a1.getEmail(), "jMayer.com");
+			assertEquals(a1.getPswd(), "pswd");
+			assertEquals(a1.getDob(), "12/01/1994");
+		}
 		
-		assertEquals(l1.getName(), "Owen Campbell");
-		assertEquals(l1.getEmail(), "yes.com");
-		assertEquals(l1.getPswd(), "pswd");
-		assertEquals(l1.getDob(), "05/02/2002");
-		
-	}
-	
-	// Tester method to identify that the attributes of the artists (Grey) 
-	@Test
-	void testArtist() {
-		Account a1 = new Account("John Mayer", "jMayer.com", "pswd", "12/01/1994", "Artist");
-		System.out.println(a1.getClass());
-		assertEquals(a1.getName(), "John Mayer");
-		assertEquals(a1.getEmail(), "jMayer.com");
-		assertEquals(a1.getPswd(), "pswd");
-		assertEquals(a1.getDob(), "12/01/1994");
-	}
-	
-	// BLACK BOX TESTS BELOW
+		// BLACK BOX TESTS BELOW
 		
 		// Tester method to check era helper method to ensure that era is correctly computed (Black)
 		@Test
@@ -44,48 +65,48 @@ class GoodTunezTester {
 			assertEquals("70s", Account.computeEra("1965"));
 			assertEquals("Current", Account.computeEra("2035"));
 		}
-	
-	// Tester method to verify that the login is functional (Black)
-	@Test
-	void loginVerificationTest() {
-		LoginPage log = new LoginPage();
-		assertTrue(log.loginVerification("jMayer.com", "pswd"));
-		assertFalse(log.loginVerification("jMayer.com", "badPswd"));
-	}
-	
-	// Tester method to verify password (Black)
-	@Test 
-	void pswdVerificationTest() {
-		CreateAccount acc = new CreateAccount();
 		
-		assertTrue(acc.verifyPswd("pswd", "pswd", "Listener"));
-		assertFalse(acc.verifyPswd("pswd", "badPswd", "false"));
-	}
-	
-	// Tester method to prove that user is able to login
-	@Test
-	void loginAbility() {
+		// Tester method to verify that the login is functional (Black)
+		@Test
+		void loginVerificationTest() {
+			LoginPage log = new LoginPage();
+			assertTrue(log.loginVerification("jMayer.com", "pswd"));
+			assertFalse(log.loginVerification("jMayer.com", "badPswd"));
+		}
 		
+		// Tester method to verify password (Black)
+		@Test 
+		void pswdVerificationTest() {
+			CreateAccount acc = new CreateAccount();
+			
+			assertTrue(acc.verifyPswd("pswd", "pswd", "Listener"));
+			assertFalse(acc.verifyPswd("pswd", "badPswd", "false"));
+		}
 		
-	}
-	
-	@Test
-	void searchBoxTest() {
-		// when the box is selected, get the item from combo box when search is hit
-		// return the information within the boxes. 
-	}
-	
-	@ Test
-	void searchBarTest() {
-		String song1 = "Bohemian Rhapsody,Queen,Album,1975,Rock";
-		String song2 = null;
+		// Tester method to prove that user is able to login
+		@Test
+		void loginAbility() {
+			
+			
+		}
 		
-		Song s1 = new Song("Bohemian Rhapsody", "Queen", "Album", 1975, "Rock");
-		Song s2 = null;
+		@Test
+		void searchBoxTest() {
+			// when the box is selected, get the item from combo box when search is hit
+			// return the information within the boxes. 
+		}
 		
-		assertTrue(s1.equals(Account.searchBarReturn(song1)));
-		assertFalse(s2.equals(Account.searchBarReturn(song1)));
-		assertEquals(song1.toString(), Account.searchBarReturn(song1).toString());
-		assertEquals("Song not in database", Account.searchBarReturn(song2).toString());
-	}
+		@ Test
+		void searchBarTest() {
+			String song1 = "Bohemian Rhapsody,Queen,Album,1975,Rock";
+			String song2 = null;
+			
+			Song s1 = new Song("Bohemian Rhapsody", "Queen", "Album", 1975, "Rock");
+			Song s2 = null;
+			
+			assertTrue(s1.equals(Account.searchBarReturn(song1)));
+			assertFalse(s2.equals(Account.searchBarReturn(song1)));
+			assertEquals(song1.toString(), Account.searchBarReturn(song1).toString());
+			assertEquals("Song not in database", Account.searchBarReturn(song2).toString());
+		}
 }
